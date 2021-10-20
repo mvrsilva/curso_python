e80d6fa7d50885592bea454a3e4b071c927df571
mvrsilva <38014996+mvrsilva@users.noreply.github.com>
2021-10-19 22:05:56 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,26 @@
+#1: Implementar uma função para cada operação
+2
+3 #1.1 Soma
+4 def somar(n1,n2):
+5 return n1+n2
+6
+7 #1.2 Subtração
+8 def diminuir(n1,n2):
+9 return n1-n2
+10
+11 #1.3 Divisao
+12 def dividir(n1,n2):
+13 if(n2 != 0):
+14 return n1/n2
+15 print('Não dividirás por zero')
+16
+17 #1.4 Multiplicação
+18 def multiplicar(n1,n2):
+19 return n1*n2
+20
+21 #2: Função principal
+22 def main():
+23
 24 #2.1: Capturar dois números:
 25 n1 = float(input("Digite o primeiro número: "))
 26 n2 = float(input("Digite o segundo número: "))
